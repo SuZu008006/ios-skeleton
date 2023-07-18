9cf202ac28b5c0bb21870d660967f9f6e093cfd3
suzusuke <suzusuke0806@gmail.com>
2023-07-18 20:47:28 +0900

    Initial Commit

---
+++ IosSkeleton/IosSkeletonApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosSkeletonApp.swift
+//  IosSkeleton
+//
+//  Created by SuZu on 2023/07/18.
+//
+
+import SwiftUI
+
+@main
+struct IosSkeletonApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
